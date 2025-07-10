6d03c3dbad99572092179a3cd2ae7c93f8471e45
Pratikshya <pratikshyagochhayat@gmail.com>
2025-07-11 01:25:33 +0530

    feat: add auth based routing

--- src/App.tsx
+++ src/App.tsx
@@ -4,21 +4,52 @@ import {
   Route,
   Navigate,
 } from "react-router-dom";
-import AddEmployee from "./pages/AddEmployee";
-import EmployeeList from "./pages/EmployeeList";
 import DashboardLayout from "./components/DashboardLayout";
 import { Toaster } from "react-hot-toast";
+import { SignInPage, SignUpPage } from "./pages/public";
+import { AddEmployee, EmployeeList } from "./pages/protected";
+import ProtectedRoute from "./components/ProtectedRoute";
+import PublicRoute from "./components/PublicRoute";
 
 function App() {
   return (
     <Router>
       <Toaster position="top-center" reverseOrder={true} />
       <Routes>
-        <Route path="/" element={<DashboardLayout />}>
+        {/* Protected Routes - Require Authentication */}
+        <Route
+          path="/"
+          element={
+            <ProtectedRoute>
+              <DashboardLayout />
+            </ProtectedRoute>
+          }
+        >
           <Route path="/add" element={<AddEmployee />} />
           <Route path="/list" element={<EmployeeList />} />
           <Route path="/" element={<Navigate to="/add" />} />
         </Route>
+
+        {/* Public Routes - Redirect authenticated users */}
+        <Route
+          path="/signin"
+          element={
+            <PublicRoute>
+              <SignInPage />
+            </PublicRoute>
+          }
+        />
+        <Route
+          path="/signup"
+          element={
+            <PublicRoute>
+              <SignUpPage />
+            </PublicRoute>
+          }
+        />
+
+        {/* Catch all route - redirect to signin */}
+        <Route path="*" element={<Navigate to="/signin" replace />} />
       </Routes>
     </Router>
   );
