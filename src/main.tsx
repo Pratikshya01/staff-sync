b07396eed3c2d42257aea438a7fa6768dc47de41
Pratikshya <pratikshyagochhayat@gmail.com>
2025-07-11 02:41:48 +0530

    refactor: use publishable key for clerk from config

--- src/main.tsx
+++ src/main.tsx
@@ -3,17 +3,18 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
 import { ClerkProvider } from "@clerk/clerk-react";
+import { CLERK_PUBLISHABLE_KEY } from "./config/index.ts";
 
-// Import your Publishable Key
-const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
-
-if (!PUBLISHABLE_KEY) {
+if (!CLERK_PUBLISHABLE_KEY) {
   throw new Error("Missing Publishable Key");
 }
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <ClerkProvider publishableKey={PUBLISHABLE_KEY} afterSignOutUrl="/signin">
+    <ClerkProvider
+      publishableKey={CLERK_PUBLISHABLE_KEY}
+      afterSignOutUrl="/signin"
+    >
       <App />
     </ClerkProvider>
   </StrictMode>
